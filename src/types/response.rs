691cfda6c28e4d43aff5e2659b736998fdcedc11
upc <upc@europeia.dev>
2024-08-21 22:30:35 -0400

    simplified dispatch header
    will probably add a separate return type later with more info? unsure

--- src/types/response.rs
+++ src/types/response.rs
@@ -4,14 +4,6 @@ use serde::Serialize;
 pub(crate) struct DispatchHeader {
     pub(crate) id: i32,
     pub(crate) nation: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub(crate) category: Option<i16>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub(crate) subcategory: Option<i16>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub(crate) title: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub(crate) created_by: Option<String>,
 }
 
 #[derive(Serialize)]
