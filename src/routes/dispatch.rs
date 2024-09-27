745f3cb94e04fc5f0976fe4b075c2261bbb49a8b
upc <upc@europeia.dev>
2024-09-26 20:39:40 -0400

    simplified return type for dispatch mod routes

--- src/routes/dispatch.rs
+++ src/routes/dispatch.rs
@@ -1,9 +1,7 @@
 use crate::core::error::Error;
 use crate::core::state::AppState;
-use axum::body::Body;
 use axum::extract::{Json, Path, State};
 use axum::http::StatusCode;
-use axum::response::{self, IntoResponse};
 use axum::Extension;
 use axum_macros::debug_handler;
 use tokio::sync::oneshot;
@@ -48,7 +46,7 @@ pub(crate) async fn post_dispatch(
     State(state): State<AppState>,
     Extension(user): Extension<User>,
     Json(params): Json<NewDispatch>,
-) -> Result<impl IntoResponse, Error> {
+) -> Result<(StatusCode, Json<i32>), Error> {
     if !user.claims.contains(&"dispatches.create".to_string()) {
         return Err(Error::Unauthorized);
     }
@@ -64,14 +62,7 @@ pub(crate) async fn post_dispatch(
         .unwrap();
 
     match rx.await {
-        Ok(Response::Success(id)) => {
-            let response = response::Response::builder()
-                .status(StatusCode::ACCEPTED)
-                .body(Body::from(id.to_string()))
-                .unwrap();
-
-            Ok(response)
-        }
+        Ok(Response::Success(id)) => Ok((StatusCode::ACCEPTED, Json(id))),
         Ok(Response::Error(e)) => Err(e),
         _ => todo!(),
     }
@@ -84,7 +75,7 @@ pub(crate) async fn edit_dispatch(
     Extension(user): Extension<User>,
     Path(id): Path<i32>,
     Json(params): Json<EditDispatch>,
-) -> Result<impl IntoResponse, Error> {
+) -> Result<(StatusCode, Json<i32>), Error> {
     if !user.claims.contains(&"dispatches.edit".to_string()) {
         return Err(Error::Unauthorized);
     }
@@ -102,14 +93,7 @@ pub(crate) async fn edit_dispatch(
         .unwrap();
 
     match rx.await {
-        Ok(Response::Success(id)) => {
-            let response = response::Response::builder()
-                .status(StatusCode::ACCEPTED)
-                .body(Body::from(id.to_string()))
-                .unwrap();
-
-            Ok(response)
-        }
+        Ok(Response::Success(id)) => Ok((StatusCode::ACCEPTED, Json(id))),
         Ok(Response::Error(e)) => Err(e),
         _ => todo!(),
     }
@@ -121,7 +105,7 @@ pub(crate) async fn remove_dispatch(
     State(state): State<AppState>,
     Extension(user): Extension<User>,
     Path(id): Path<i32>,
-) -> Result<impl IntoResponse, Error> {
+) -> Result<(StatusCode, Json<i32>), Error> {
     if !user.claims.contains(&"dispatches.delete".to_string()) {
         return Err(Error::Unauthorized);
     }
@@ -139,14 +123,7 @@ pub(crate) async fn remove_dispatch(
         .unwrap();
 
     match rx.await {
-        Ok(Response::Success(id)) => {
-            let response = response::Response::builder()
-                .status(StatusCode::ACCEPTED)
-                .body(Body::from(id.to_string()))
-                .unwrap();
-
-            Ok(response)
-        }
+        Ok(Response::Success(id)) => Ok((StatusCode::ACCEPTED, Json(id))),
         Ok(Response::Error(e)) => Err(e),
         _ => todo!(),
     }
